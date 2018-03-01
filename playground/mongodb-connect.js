9328c4adab857e800af4e6bf513e5d5652c3a922
DilipKunderu <dilipkunderu@gmail.com>
2018-02-28 23:29:29 -0500

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,18 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to mongodb server');
+    }
+    console.log('Connected to the MongoDB server');
+    db.collection('Todos').find({completed: true}).toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+    //db.close();
+} );
